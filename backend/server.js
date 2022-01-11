a23f9b8955922b751f8b16662f00c37206fd9430
m-shehzad <shehzadboii@gmail.com>
2022-01-11 14:38:29 +0530

    order functionality added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { send } = require("express/lib/response");
 
 const app = express();
 
@@ -35,13 +34,9 @@ app.get('/menuItem',(req,res)=>{
   let sql = 'SELECT * FROM MENU';
   db.query(sql,(err,result)=>{
     if(err) throw err;
-    res.send(result);
+    res.json(result);
   });
 });
-
-app.get('/shit',(req,res)=>{
-  res.send('yo this shit works!');
-})
 
 app.post('/login',(req,res)=>{
   console.log(req.body)
@@ -71,10 +66,49 @@ app.post('/register',(req,res)=>{
     else{
       db.query(`INSERT INTO registry VALUES ('${username}','${password}','${email}')`,(err,result)=>{
         if(err) throw err;
-        res.redirect('/menu');
+        res.redirect('/');
       })
     }
   })
+})
+
+app.post('/order',(req,res)=>{
+  const {ITEMS,address,payment_type,user} =req.body;
+  console.log(ITEMS);
+  console.log(user);
+  console.log(address);
+  console.log(payment_type);
+  //initializing the order with cust_id
+  let sql = `INSERT INTO ORDERS (CUST_ID)
+    SELECT (CUST_ID) FROM customer
+    WHERE USERNAME='${user}';`
+  db.query(sql,(err,result)=>{
+    if(err) throw err;
+    //mapping all the items ordered
+    ITEMS.map(item=>{
+      sql=`INSERT INTO order_from values(LAST_INSERT_ID(),'${item.ITEM_NAME}',${item.quantity});`;
+      db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log('item inserted!');
+      })
+    })
+    //inserting the miscellenous values
+    sql =`UPDATE orders
+    SET ORDER_TIME=NOW(),ADDRESS='${address}'
+    WHERE ORDER_ID = LAST_INSERT_ID();`;
+    db.query(sql,(err,result)=>{
+      if (err) throw err;
+      console.log('Order Inserted!');
+    })
+    
+    sql=`INSERT INTO payment values(LAST_INSERT_ID(),'${payment_type}',NULL);`;
+    db.query(sql,(err,result)=>{
+      if(err) throw err;
+      console.log('payment sucessful!');  
+    })
+
+  })
+
 })
 
 app.listen(4000,()=>{
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 import './CartItem';
 import './Cart.css';
 import CartItem from './CartItem';
-
+import { UserContext } from '../../UserContext';
 export default function Cart(props){
-
+    let navigate = useNavigate();
+    const [logState,setLogState] = useContext(UserContext);
     const totalPrice = props.cartItems.reduce((price,item)=>price+item.quantity*item.PRICE,0)
+    const [address,setAddress] = useState('');
+    const [payment,setPayment] = useState('Cash');
 
+
+    async function order(){
+        if (logState){
+            let res = await fetch('/order',{
+                method:'POST',
+                headers:{
+                    'content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                ITEMS:props.cartItems,
+                user:logState,
+                address:address,
+                payment_type:payment
+            })
+            })
+            let data = await res.json()
+            if(data=='sucess'){
+                alert('Your Order has been registered!');
+            }
+        }
+        else{
+            alert('You are not Logged in!');
+            navigate('/login');
+        }
+    }
+    
     return(<section className="cart">
     <h1 className="heading">Your Orders</h1>
     <div className="header">
         <h3 className="cart-title">Orders</h3>
-        <h5 className="Action">Remove all</h5>
+        <h5 onClick={()=>props.cartClearance()} className="Action">Remove all</h5>
     </div>
     {props.cartItems.length===0 && (
         <h1 className="noitems">No items are added.</h1>
-    )}
+        )}
     {props.cartItems.map((item)=>{
+        console.log(...props.cartItems)
         return (<CartItem 
         key = {item.ITEM_NAME}
         ITEM_NAME = {item.ITEM_NAME}
@@ -25,10 +56,27 @@ export default function Cart(props){
         addProduct = {props.addProduct}
         removeProduct = {props.removeProduct}
         />)
+
     })}
-    {props.cartItems.length!==0 &&(<div className="checkout">
-        <h1 className="subtotal">sub total</h1>
+    {props.cartItems.length!==0 
+    &&(<><div className="checkout">
+        <h1 className="checkout-title">sub total</h1>
         <h1 className="total-amount"><i className="fas fa-rupee-sign"></i>{totalPrice}</h1>
-    </div>)}
+        </div>
+        <div className='addresspay'>
+            <span className='subheading'>Payment</span>
+            <select onChange={(e)=>setPayment(e.target.value)} name="payment" id="pay-type" value={payment} required>
+                <option value="Cash">Cash</option>
+                <option value="Debit">Debit</option>
+                <option value="UPI">UPI</option>
+            </select>
+            <h1>Address</h1>
+            <textarea onChange={(e)=>setAddress(e.target.value)} name="address" id="address" cols="30" rows="10" value={address} required></textarea>
+        </div>
+        <div className='order-btn'>
+        <button onClick={()=>order()} className=' btn'>Order</button>
+        </div>
+        </>)}
+        
 </section>)
 }
